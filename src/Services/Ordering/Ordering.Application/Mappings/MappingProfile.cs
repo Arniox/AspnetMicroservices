90e771702bda2755d7a881f7f27be375abcacdb6
Nikkolas Diehl <nikkdiehl@gmail.com>
2021-06-08 15:57:27 +1200

    Added: Domain/Common: - ValueObject abstract function class - EntityBase abstract class for all entities
    Domain/Entities:
    - Added Order Entity Class that inherits from EntityBase class
    
    Application/Mappings:
    - Added MappingProfile to map Order's to OrdersVm (View Model) and visa versa
    
    Application/Models:
    - Added Email Model
    - Added EmailSettings Model
    
    Application/Contracts/Infrastructure:
    - IEmailService interface for sending Emails
    
    Application/Contracts/Persistence:
    - IAsyncRepository interface for getting any Entity that inherits from EntityBase
    - IOrderRepository interface that inherits from IAsyncRepository interface with the EntityType = Order. This interface sets up GetOrdersByUserName EndPoint
    
    Application/Features/Orders/Queries/GetOrdersList:
    - GetOrdersListQuery that inherits the MediatR IRequest object with typename of List<OrdersVm> and sets UserName
    - GetOrdersListQueryHandler that inherits MediatR IRequestHandler with typename of GetOrdersListQuery, List<OrdersVm>. Injecting both GetOrdersListQuery and OrdersVm.
    - OrdesVm is a view model version of the Orders Entity.

---
+++ src/Services/Ordering/Ordering.Application/Mappings/MappingProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Ordering.Application.Features.Orders.Queries.GetOrdersList;
+using Ordering.Domain.Entities;
+
+namespace Ordering.Application.Mappings
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            //Set up mapping profiles for visa versa object mapping
+            CreateMap<Order, OrdersVm>().ReverseMap();
+        }
+    }
+}
