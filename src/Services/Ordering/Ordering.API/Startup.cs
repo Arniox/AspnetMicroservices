fb10c1798bbd1328cec1c8686b6a78fe1aff317d
Nikkolas Diehl <nikkdiehl@gmail.com>
2021-06-09 14:26:44 +1200

    Added ApplicationServicesRegistration IServiceCollection extension class. - Auto adds AutoMapper, Validators, and MediatR pipeline - Auto adds pipelines (ExceptionBehaviour)

--- src/Services/Ordering/Ordering.API/Startup.cs
+++ src/Services/Ordering/Ordering.API/Startup.cs
@@ -25,6 +25,8 @@ namespace Ordering.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //Add repository for business layer
+            //services.AddApplicationServices();
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
---
+++ src/Services/Ordering/Ordering.Application/ApplicationServiceRegistration.cs
@@ -0,0 +1,26 @@
+﻿using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using Ordering.Application.Behaviours;
+using System.Reflection;
+
+namespace Ordering.Application
+{
+    public static class ApplicationServiceRegistration
+    {
+        //Extenion Method
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            //Add all services
+            services.AddAutoMapper(Assembly.GetExecutingAssembly()); //Looks for custom Mapping Profiles (instance of Profile)
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly()); //Looks for any validator files (instances of AbstractValidator)
+            services.AddMediatR(Assembly.GetExecutingAssembly()); //Looks for any Request files (instances of IRequest or IRequestHandler)
+
+            //Add all pipeline behaviours
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(UnhandledExceptionBehaviour<,>));
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehaviour<,>));
+
+            return services;
+        }
+    }
+}
