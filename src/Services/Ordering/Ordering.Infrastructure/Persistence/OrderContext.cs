decb47b8f871e7d750d72db50e94c113eecb26c9
Nikkolas Diehl <nikkdiehl@gmail.com>
2021-06-09 15:17:16 +1200

    Added OrderContent (Database Context) Added OrderContextSeed (Database Seeding on Startup)

---
+++ src/Services/Ordering/Ordering.Infrastructure/Persistence/OrderContext.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Ordering.Domain.Common;
+using Ordering.Domain.Entities;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Ordering.Infrastructure.Persistence
+{
+    //Database context
+    public class OrderContext : DbContext
+    {
+        //Constructor
+        public OrderContext(DbContextOptions<OrderContext> options) : base(options)
+        { 
+        }
+
+        //Add sets
+        public DbSet<Order> Orders { get; set; }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            //For each entity in the ChangeTracker (any items modifed or added)
+            //Set entity CreatedDate / CreatedBy
+            //Set entity LastModifiedDate / LastModifiedBy
+            foreach(var entry in ChangeTracker.Entries<EntityBase>())
+            {
+                switch (entry.State)
+                {
+                    case EntityState.Added:
+                        entry.Entity.CreatedDate = DateTime.Now;
+                        entry.Entity.CreatedBy = "Arniox";
+                        break;
+                    case EntityState.Modified:
+                        entry.Entity.LastModifiedDate = DateTime.Now;
+                        entry.Entity.LastModifiedBy = "Arniox";
+                        break;
+                }
+            }
+
+            return base.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
---
+++ src/Services/Ordering/Ordering.Infrastructure/Persistence/OrderContextSeed.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.Logging;
+using Ordering.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Infrastructure.Persistence
+{
+    public class OrderContextSeed
+    {
+        //Seed
+        public static async Task SeedAsync(OrderContext orderContext, ILogger<OrderContextSeed>)
+        {
+            //If any any record already
+            if (!orderContext.Orders.Any())
+            {
+                orderContext.Orders.AddRange(GetPreconfiguredOrders());
+                //Save
+                await orderContext.SaveChangesAsync();
+            }
+        }
+
+        //Get pre-data
+        private static IEnumerable<Order> GetPreconfiguredOrders()
+        {
+            //Return new list of Orders
+            return new List<Order>
+            {
+                new Order() { UserName = "Arniox", FirstName = "Nikkolas", LastName = "Diehl", EmailAddress = "nikkdiehl@gmail.com", AddressLine = "Henderson", Country = "New Zealand", TotalPrice = 350 }
+            };
+        }
+    }
+}
