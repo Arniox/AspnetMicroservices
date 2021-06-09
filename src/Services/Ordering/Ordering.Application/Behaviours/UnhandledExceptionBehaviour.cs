4c9dfcfdb9313bf01898825304f4df0ed456dcc3
Nikkolas Diehl <nikkdiehl@gmail.com>
2021-06-09 14:06:11 +1200

    Added Behaviour for UnhandledException

---
+++ src/Services/Ordering/Ordering.Application/Behaviours/UnhandledExceptionBehaviour.cs
@@ -0,0 +1,37 @@
+﻿using MediatR;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Behaviours
+{
+    public class UnhandledExceptionBehaviour<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+    {
+        //Objects
+        private readonly ILogger<TRequest> _logger;
+
+        //Constructor
+        public UnhandledExceptionBehaviour(ILogger<TRequest> logger)
+        {
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, 
+            RequestHandlerDelegate<TResponse> next)
+        {
+            //Try catch on every pipleline
+            try
+            {
+                return await next();
+            }
+            catch (Exception ex)
+            {
+                //Catch any unhandled error
+                var requestName = typeof(TRequest).Name;
+                _logger.LogError(ex, "Application Request: Unhandled Exception for Request {Name} {@Request}", requestName, request);
+                throw;
+            }
+        }
+    }
+}
--- src/Services/Ordering/Ordering.Application/Behaviours/ValidationBehaviour.cs
+++ src/Services/Ordering/Ordering.Application/Behaviours/ValidationBehaviour.cs
@@ -3,9 +3,9 @@ using MediatR;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using ValidationException = Ordering.Application.Exceptions.ValidationException;
 
 namespace Ordering.Application.Behaviours
 {
