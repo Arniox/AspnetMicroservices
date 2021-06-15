3abd51605d10ef29bd62c5cbe945b493bc3896eb
Nikkolas Diehl <nikkdiehl@gmail.com>
2021-06-15 16:18:02 +1200

    Added Model Entities to Receive Data from the API's - Added BasketItemExtendedModel - Added BasketModel - Added CatalogModel - Added OrderResponseModel - Added ShoppingModel

---
+++ src/ApiGateways/Shopping.Aggregator/Models/BasketModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Shopping.Aggregator.Models
+{
+    //From ShoppingCart Entity 
+    public class BasketModel
+    {
+        public string UserName { get; set; }
+        public List<BasketItemExtendedModel> Items { get; set; } = new List<BasketItemExtendedModel>();
+        public decimal TotalPrice { get; set; }
+    }
+}
